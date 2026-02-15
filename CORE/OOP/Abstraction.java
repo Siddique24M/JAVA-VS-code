8c980613366812d95c05b94e43015689817e0d0b
Siddique24M <siddiquemujawar@gmail.com>
2026-02-15 21:45:14 +0530

    added comments

--- CORE/OOP/Abstraction.java
+++ CORE/OOP/Abstraction.java
@@ -1,14 +1,24 @@
+//abstract class
 abstract class Mobile{
+    //abstract method
     public abstract void phone();
 }
+
+//concrete class
 public class Abstraction extends Mobile{
     @Override
     public void phone(){
-        System.out.println("open dial-pad");
+        System.out.println("open dial-pad");  //golden java rule
     }
+
+    //concrete method
     public static void main(String[] args) {
-        // Mobile m = new Mobile();
-        Mobile m = new Abstraction(); 
+        // Mobile m = new Mobile();  --cant create obj of abstract class
+        Mobile m1; //but we can create reference variable of abstract class
+
+        Mobile m = new Abstraction(); //upcasting
+
+        //runtime polymorphism
         m.phone();
     }
 }
