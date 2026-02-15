80bed30b114fb1164215bc73e92cfd47fbe7c923
Siddique24M <siddiquemujawar@gmail.com>
2026-02-15 21:39:36 +0530

    ABSTRACTION

--- CORE/OOP/Abstraction.java
+++ CORE/OOP/Abstraction.java
@@ -1,3 +1,14 @@
-public class Abstraction {
-    
+abstract class Mobile{
+    public abstract void phone();
+}
+public class Abstraction extends Mobile{
+    @Override
+    public void phone(){
+        System.out.println("open dial-pad");
+    }
+    public static void main(String[] args) {
+        // Mobile m = new Mobile();
+        Mobile m = new Abstraction(); 
+        m.phone();
+    }
 }
