a3fada18075419905c8fda38ec33d534e766ba76
Siddique24M <siddiquemujawar@gmail.com>
2026-02-17 22:58:47 +0530

    added comments

--- CORE/OOP/Method_passing.java
+++ CORE/OOP/Method_passing.java
@@ -1,13 +1,14 @@
 public class Method_passing {
     int b;
-    public static void display(Object m2){  
+    public static void display(Object m2){        //implicitly upcasting
         System.out.println(m2);
-        // System.out.println(m2.b);
-        Method_passing m1= (Method_passing)m2;
+        // System.out.println(m2.b);       class cast exception
+        Method_passing m1= (Method_passing)m2;    //explicit downcasting
         System.out.println(m1.b);
     }
     public static void main(String[] args) {
-        Method_passing m = new Method_passing();
+        Method_passing m = new Method_passing();     //creating object of class
+        m.b=10;     //assigning value to variable b
         System.out.println(m);
         System.out.println(m.b);
         display(m);
