a9d39512bd765e76184f9f78d3300ad94dc8d568
Siddique24M <siddiquemujawar@gmail.com>
2026-02-20 22:26:18 +0530

    Interface2

--- CORE/OOP/Interface1.java
+++ CORE/OOP/Interface1.java
@@ -2,27 +2,39 @@ interface A {
     void m1();
 }
 interface B extends A{
-    void m2();
+    public static void m2(){
+        System.out.println("Static method in Interface");
+    }
 }
-class C{
-    public void m3(){
+interface C{
+    public default void m3(){
+        System.out.println("Default method in interface");
+    }
+}
+class D{
+    public void m4(){
         System.out.println("class method");
     }
 }
-public class Interface1 extends C implements A,B {
+public class Interface1 extends D implements A,B,C {
     @Override
     public void m1() {
         System.out.println("A interface");
         
     }
-    @Override
-    public void m2() {
+    
+    public static void m2() {
         System.out.println("B interface");
         
+    }
+   
+    public void m3(){
+        System.out.println("C interface");
     }
     public static void main(String[] args) {
         Interface1 I = new Interface1();
         I.m1();
-        I.m2();
+        B.m2();
+        I.m3();
     }
 }
