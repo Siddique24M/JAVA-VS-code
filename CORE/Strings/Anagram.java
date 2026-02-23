39102ab64f158be2e413d7aaf34b7a48c14d15fe
Siddique24M <siddiquemujawar@gmail.com>
2026-02-23 13:54:11 +0530

    Anagram

---
+++ CORE/Strings/Anagram.java
@@ -0,0 +1,19 @@
+//silent == listen
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        System.out.println(check("silent","listen"));
+    }
+    public static boolean check(String str1, String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        char[] a = str1.toCharArray();
+        char[] b = str2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+        return Arrays.equals(a,b);
+    }
+}
