aeb56dec27b26aa68291583baf021b3c94fe95b7
Siddique24M <siddiquemujawar@gmail.com>
2026-03-24 18:04:41 +0530

    t1 2026 shift 1

---
+++ Collections/ArrayList/basic.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class basic {
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList<>();
+        a.add(30);
+        a.add(40);
+
+        ArrayList b = new ArrayList<>();
+        a.add(10);
+        a.add(20);
+        b.addAll(a);
+        for( Object o: b){
+            System.out.println(o);
+        }
+    }
+}
---
+++ TCS/2026Questions/TransactionMonitor.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class TransactionMonitor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<String> seen = new HashSet<>();
+        int prevTime = -1;
+        for(int i=0; i<n; i++){
+            String sender = sc.next(); 
+            String reciever = sc.next(); 
+            int timestamp = sc.nextInt(); 
+            int amount = sc.nextInt(); 
+
+            //rule 1- duplicate check
+            String key = sender + "-" + reciever;
+            if(seen.contains(key)){
+                System.out.println("Error: Duplicate Transaction");
+                return;
+            }
+            seen.add(key);
+
+            //rule 2- fraud detecction
+            if(prevTime !=-1 && (timestamp-prevTime>60)){
+                System.out.println("fraud detected");
+                return;
+            }
+            prevTime = timestamp;
+        }
+        System.out.println("All transaction valid");
+    }
+}
